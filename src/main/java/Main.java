8bba334ac873718e45cee95a1e6e6eda649bd9a9
Danny Cao <dannycao1997@yahoo.com>
2024-02-29 19:18:50 -0500

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
@@ -16,12 +18,16 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    public static int sumOfNumbers(int n) {
+        int totalSum = 0;
+        for (int i = 0; i <= n; i++) {
+            totalSum += i;
+        }
+        return totalSum;
     }
-    
-    
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+
+
+    public static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
