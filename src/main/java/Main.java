05a03dd16e8c37758dee7a6e9d46541dec2a6faf
Danny Cao <dannycao1997@yahoo.com>
2024-03-04 20:29:36 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,45 +5,36 @@ import java.util.Scanner;
  * Modified by kristofer in Jan 2024
  */
 public class Main {
-
     public static void main(String[] args){
-        
         int number = getInputNumber();
-        
         int sum = sumOfNumbers(number);
-        
         System.out.println(sum);
-
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
     public static int sumOfNumbers(int n) {
-        int totalSum = 0;
+        int result = 0;
         for (int i = 0; i <= n; i++) {
-            totalSum += i;
+            result += i;
         }
-        return totalSum;
+        return result;
     }
 
 
     public static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
-
         // This method reads the number provided using keyboard
-        int num = scan.nextInt();
 
-        // Closing Scanner after the use
-        scan.close();
+        int num = scan.nextInt();
+        scan.close(); // Closing Scanner after the use
         return num;
     }
-    
-    
-    
+
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
-        return 0;
+        return n *(n + 1) / 2;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
